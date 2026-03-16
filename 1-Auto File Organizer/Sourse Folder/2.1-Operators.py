48fbf829d93a5d79521f7adbd08a75b260a9f89a
Xlbooster <xlbooster.tech@gmail.com>
2026-03-16 21:27:24 +0530

    Add files via upload

---
+++ 1-Auto File Organizer/Sourse Folder/2.1-Operators.py
@@ -0,0 +1,37 @@
+#1 . Arithmatic Operators
+a=25
+b=5
+print(a+b)
+print(a/b)
+
+#2. Assignment Operators
+x=15
+print(x)
+x+=5
+print(x)
+
+#3. Comparison Operators
+z1=10
+z2=10
+print(z1==z2)
+
+z3=10
+z4=15
+print(z3!=z4)
+
+#4. Logical Operators
+a1=10
+a2=20
+a3=30
+a4=50
+
+print(a1<a2 and a3<a4)
+print("OR Result : ", a1>a2 or a3>a4)
+
+#5. Identify Operators
+b1=25
+b2=26
+print(b1 is b2)
+
+#6. Membership Operators
+print("b in apple :", 'b' in 'apple')
---
+++ 1-Auto File Organizer/Sourse Folder/VariableNames.py
@@ -0,0 +1,31 @@
+studentName="Ajay" #Camel Case
+StudentName="Ajay" #Pascal Case
+student_name="Ajay" #Snake Case
+name="Ajay" #Flat Case
+
+
+name="ajay"
+_age=18
+
+student_result_2025=365
+
+#if=6546
+#for="new"
+
+C_NAME="Ganesh"
+c_name="Vijay"
+
+
+print("Welcome",c_name)
+
+a=10
+b=3.5
+c=3+4j
+
+z1=1+3j
+z2=2+4j
+z3=z1+z2
+
+print(z3)
+
+print(type(c))
